87f93c9c219155983f1407109f3c4b6919ddb297
Haytham Mohamed <haybu@hotmail.com>
2020-02-20 00:16:38 -0600

    update

--- sql/checkout-workflow-1pod.sql
+++ sql/checkout-workflow-1pod.sql
@@ -1,15 +1,28 @@
 
+select 'svc_req_rate' as variable, time, sum(round(cast(COALESCE(value,'NaN') as numeric) ,2)) as val
+from (select distinct time, value, name, labels from metrics 
+		where name = 'service:X_requests_total:sum_rate') as xmetrics
+where name = 'service:X_requests_total:sum_rate' 
+and labels ->> 'destination_service' in ('checkoutservice.default.svc.cluster.local')
+--and time between '2020-02-17 08:36:00'::timestamp and '2020-02-17 23:36:00'::timestamp
+and (time between '2020-02-17 08:36:21'::timestamp and '2020-02-17 12:22:17'::timestamp 
+	or time between '2020-02-17 13:54:00'::timestamp and '2020-02-17 15:36:50'::timestamp
+	or time between '2020-02-17 15:40:00'::timestamp and '2020-02-17 19:09:17'::timestamp
+	or time between '2020-02-17 19:17:10'::timestamp and '2020-02-17 23:36:00'::timestamp)
+group by variable, time
+order by time
+;
+
 select 'svc_cpu_use' as variable, time, sum(round(cast(COALESCE(value,'NaN') as numeric) ,2)) as val
 from (select distinct time, value, name, labels from metrics 
 		where name = 'service:X_cpu_usage_total_seconds:sum_rate') as xmetrics
 where name = 'service:X_cpu_usage_total_seconds:sum_rate' 
 and labels ->> 'label_app' in ('checkoutservice','cartservice','emailservice','currencyservice'
 	,'paymentservice','productcatalogservice','shippingservice')
-and time between '2020-02-17 08:36:00'::timestamp and '2020-02-17 23:36:00'::timestamp
---and (time between '2020-02-17 08:36:21'::timestamp and '2020-02-17 12:22:17'::timestamp 
---	or time between '2020-02-17 13:54:00'::timestamp and '2020-02-17 15:36:50'::timestamp
---	or time between '2020-02-17 15:40:00'::timestamp and '2020-02-17 19:09:17'::timestamp
---	or time between '2020-02-17 19:17:10'::timestamp and '2020-02-17 23:36:00'::timestamp)
+and (time between '2020-02-17 08:36:21'::timestamp and '2020-02-17 12:22:17'::timestamp 
+	or time between '2020-02-17 13:54:00'::timestamp and '2020-02-17 15:36:50'::timestamp
+	or time between '2020-02-17 15:40:00'::timestamp and '2020-02-17 19:09:17'::timestamp
+	or time between '2020-02-17 19:17:10'::timestamp and '2020-02-17 23:36:00'::timestamp)
 group by variable, time
 order by time
 ;
@@ -20,7 +33,10 @@ from (select distinct time, value, name, labels from metrics
 where name = 'service:X_cpu_saturation_total_seconds:sum_rate' 
 and labels ->> 'label_app' in ('checkoutservice','cartservice','emailservice','currencyservice'
 	,'paymentservice','productcatalogservice','shippingservice')
-and time between '2020-02-17 08:36:00'::timestamp and '2020-02-17 23:36:00'::timestamp
+and (time between '2020-02-17 08:36:21'::timestamp and '2020-02-17 12:22:17'::timestamp 
+	or time between '2020-02-17 13:54:00'::timestamp and '2020-02-17 15:36:50'::timestamp
+	or time between '2020-02-17 15:40:00'::timestamp and '2020-02-17 19:09:17'::timestamp
+	or time between '2020-02-17 19:17:10'::timestamp and '2020-02-17 23:36:00'::timestamp)
 group by variable, time
 order by time
 ;
@@ -31,7 +47,10 @@ from (select distinct time, value, name, labels from metrics
 where name = 'service:X_network_usage_bytes_total:sum_rate' 
 and labels ->> 'label_app' in ('checkoutservice','cartservice','emailservice','currencyservice'
 	,'paymentservice','productcatalogservice','shippingservice')
-and time between '2020-02-17 08:36:00'::timestamp and '2020-02-17 23:36:00'::timestamp 
+and (time between '2020-02-17 08:36:21'::timestamp and '2020-02-17 12:22:17'::timestamp 
+	or time between '2020-02-17 13:54:00'::timestamp and '2020-02-17 15:36:50'::timestamp
+	or time between '2020-02-17 15:40:00'::timestamp and '2020-02-17 19:09:17'::timestamp
+	or time between '2020-02-17 19:17:10'::timestamp and '2020-02-17 23:36:00'::timestamp)
 group by variable, time
 order by time
 ;
@@ -42,7 +61,10 @@ from (select distinct time, value, name, labels from metrics
 where name = 'service:X_disk_usage_total_byte:sum_rate' 
 and labels ->> 'label_app' in ('checkoutservice','cartservice','emailservice','currencyservice'
 	,'paymentservice','productcatalogservice','shippingservice')
-and time between '2020-02-17 08:36:00'::timestamp and '2020-02-17 23:36:00'::timestamp
+and (time between '2020-02-17 08:36:21'::timestamp and '2020-02-17 12:22:17'::timestamp 
+	or time between '2020-02-17 13:54:00'::timestamp and '2020-02-17 15:36:50'::timestamp
+	or time between '2020-02-17 15:40:00'::timestamp and '2020-02-17 19:09:17'::timestamp
+	or time between '2020-02-17 19:17:10'::timestamp and '2020-02-17 23:36:00'::timestamp)
 group by variable, time
 order by time
 ;
@@ -51,7 +73,10 @@ select 'system_cpu_use' as variable, time, round(cast(COALESCE(value,'NaN') as n
 from (select distinct time, value, name, labels from metrics 
 		where name = 'system:X_cpu_usage_total_seconds:sum_rate') as xmetrics
 where name = 'system:X_cpu_usage_total_seconds:sum_rate' 
-and time between '2020-02-17 08:36:00'::timestamp and '2020-02-17 23:36:00'::timestamp
+and (time between '2020-02-17 08:36:21'::timestamp and '2020-02-17 12:22:17'::timestamp 
+	or time between '2020-02-17 13:54:00'::timestamp and '2020-02-17 15:36:50'::timestamp
+	or time between '2020-02-17 15:40:00'::timestamp and '2020-02-17 19:09:17'::timestamp
+	or time between '2020-02-17 19:17:10'::timestamp and '2020-02-17 23:36:00'::timestamp)
 order by time
 ;
 
@@ -59,7 +84,10 @@ select 'system_cpu_sat' as variable, time, round(cast(COALESCE(value,'NaN') as n
 from (select distinct time, value, name, labels from metrics 
 		where name = 'system:X_cpu_saturation_total_seconds:sum') as xmetrics
 where name = 'system:X_cpu_saturation_total_seconds:sum' 
-and time between '2020-02-17 08:36:00'::timestamp and '2020-02-17 23:36:00'::timestamp
+and (time between '2020-02-17 08:36:21'::timestamp and '2020-02-17 12:22:17'::timestamp 
+	or time between '2020-02-17 13:54:00'::timestamp and '2020-02-17 15:36:50'::timestamp
+	or time between '2020-02-17 15:40:00'::timestamp and '2020-02-17 19:09:17'::timestamp
+	or time between '2020-02-17 19:17:10'::timestamp and '2020-02-17 23:36:00'::timestamp)
 order by time
 ;
 
@@ -67,7 +95,10 @@ select 'system_net_use' as variable, time, round(cast(COALESCE(value,'NaN') as n
 from (select distinct time, value, name, labels from metrics 
 		where name = 'system:X_network_usage_bytes_total:sum_rate') as xmetrics
 where name = 'system:X_network_usage_bytes_total:sum_rate' 
-and time between '2020-02-17 08:36:00'::timestamp and '2020-02-17 23:36:00'::timestamp
+and (time between '2020-02-17 08:36:21'::timestamp and '2020-02-17 12:22:17'::timestamp 
+	or time between '2020-02-17 13:54:00'::timestamp and '2020-02-17 15:36:50'::timestamp
+	or time between '2020-02-17 15:40:00'::timestamp and '2020-02-17 19:09:17'::timestamp
+	or time between '2020-02-17 19:17:10'::timestamp and '2020-02-17 23:36:00'::timestamp)
 order by time
 ;
 
@@ -75,7 +106,10 @@ select 'system_net_sat' as variable, time, round(cast(COALESCE(value,'NaN') as n
 from (select distinct time, value, name, labels from metrics 
 		where name = 'system:X_network_saturation_bytes_total:sum_rate') as xmetrics
 where name = 'system:X_network_saturation_bytes_total:sum_rate' 
-and time between '2020-02-17 08:36:00'::timestamp and '2020-02-17 23:36:00'::timestamp
+and (time between '2020-02-17 08:36:21'::timestamp and '2020-02-17 12:22:17'::timestamp 
+	or time between '2020-02-17 13:54:00'::timestamp and '2020-02-17 15:36:50'::timestamp
+	or time between '2020-02-17 15:40:00'::timestamp and '2020-02-17 19:09:17'::timestamp
+	or time between '2020-02-17 19:17:10'::timestamp and '2020-02-17 23:36:00'::timestamp)
 order by time
 ;
 
@@ -83,7 +117,10 @@ select 'system_disk_sat' as variable, time, round(cast(COALESCE(value,'NaN') as 
 from (select distinct time, value, name, labels from metrics 
 		where name = 'system:X_disk_usage_bytes_total:sum_relative') as xmetrics
 where name = 'system:X_disk_usage_bytes_total:sum_relative' 
-and time between '2020-02-17 08:36:00'::timestamp and '2020-02-17 23:36:00'::timestamp
+and (time between '2020-02-17 08:36:21'::timestamp and '2020-02-17 12:22:17'::timestamp 
+	or time between '2020-02-17 13:54:00'::timestamp and '2020-02-17 15:36:50'::timestamp
+	or time between '2020-02-17 15:40:00'::timestamp and '2020-02-17 19:09:17'::timestamp
+	or time between '2020-02-17 19:17:10'::timestamp and '2020-02-17 23:36:00'::timestamp)
 order by time
 ;
 
@@ -91,7 +128,10 @@ select 'system_disk_io' as variable, time, round(cast(COALESCE(value,'NaN') as n
 from (select distinct time, value, name, labels from metrics 
 		where name = 'system:X_disk_io_total_seconds:sum_rate') as xmetrics
 where name = 'system:X_disk_io_total_seconds:sum_rate' 
-and time between '2020-02-17 08:36:00'::timestamp and '2020-02-17 23:36:00'::timestamp
+and (time between '2020-02-17 08:36:21'::timestamp and '2020-02-17 12:22:17'::timestamp 
+	or time between '2020-02-17 13:54:00'::timestamp and '2020-02-17 15:36:50'::timestamp
+	or time between '2020-02-17 15:40:00'::timestamp and '2020-02-17 19:09:17'::timestamp
+	or time between '2020-02-17 19:17:10'::timestamp and '2020-02-17 23:36:00'::timestamp)
 order by time
 ;
 
@@ -100,7 +140,10 @@ from (select distinct time, value, name, labels from metrics
 		where name = 'service:X_95th_latency_seconds:sum_rate') as xmetrics
 where name = 'service:X_95th_latency_seconds:sum_rate' 
 and labels ->> 'destination_service_name' in ('checkoutservice')
-and time between '2020-02-17 08:36:00'::timestamp and '2020-02-17 23:36:00'::timestamp
+and (time between '2020-02-17 08:36:21'::timestamp and '2020-02-17 12:22:17'::timestamp 
+	or time between '2020-02-17 13:54:00'::timestamp and '2020-02-17 15:36:50'::timestamp
+	or time between '2020-02-17 15:40:00'::timestamp and '2020-02-17 19:09:17'::timestamp
+	or time between '2020-02-17 19:17:10'::timestamp and '2020-02-17 23:36:00'::timestamp)
 order by time
 ;
 
@@ -109,7 +152,10 @@ from (select distinct time, value, name, labels from metrics
 		where name = 'service:X_95th_latency_success_seconds:sum_rate') as xmetrics
 where name = 'service:X_95th_latency_success_seconds:sum_rate' 
 and labels ->> 'destination_service_name' in ('checkoutservice')
-and time between '2020-02-17 08:36:00'::timestamp and '2020-02-17 23:36:00'::timestamp
+and (time between '2020-02-17 08:36:21'::timestamp and '2020-02-17 12:22:17'::timestamp 
+	or time between '2020-02-17 13:54:00'::timestamp and '2020-02-17 15:36:50'::timestamp
+	or time between '2020-02-17 15:40:00'::timestamp and '2020-02-17 19:09:17'::timestamp
+	or time between '2020-02-17 19:17:10'::timestamp and '2020-02-17 23:36:00'::timestamp)
 order by time
 ;
 
@@ -120,7 +166,10 @@ where name = 'service:X_request_size_bytes:sum_rate'
 and labels ->> 'destination_service' in ('checkoutservice.default.svc.cluster.local','cartservice.default.svc.cluster.local'
     ,'emailservice.default.svc.cluster.local','currencyservice.default.svc.cluster.local','paymentservice.default.svc.cluster.local'
     ,'productcatalogservice.default.svc.cluster.local','shippingservice.default.svc.cluster.local')
-and time between '2020-02-17 08:36:00'::timestamp and '2020-02-17 23:36:00'::timestamp
+and (time between '2020-02-17 08:36:21'::timestamp and '2020-02-17 12:22:17'::timestamp 
+	or time between '2020-02-17 13:54:00'::timestamp and '2020-02-17 15:36:50'::timestamp
+	or time between '2020-02-17 15:40:00'::timestamp and '2020-02-17 19:09:17'::timestamp
+	or time between '2020-02-17 19:17:10'::timestamp and '2020-02-17 23:36:00'::timestamp)
 group by variable, time
 order by time
 ;
@@ -132,7 +181,10 @@ where name = 'service:X_response_size_bytes:sum_rate'
 and labels ->> 'destination_service' in ('checkoutservice.default.svc.cluster.local','cartservice.default.svc.cluster.local'
     ,'emailservice.default.svc.cluster.local','currencyservice.default.svc.cluster.local','paymentservice.default.svc.cluster.local'
     ,'productcatalogservice.default.svc.cluster.local','shippingservice.default.svc.cluster.local')
-and time between '2020-02-17 08:36:00'::timestamp and '2020-02-17 23:36:00'::timestamp
+and (time between '2020-02-17 08:36:21'::timestamp and '2020-02-17 12:22:17'::timestamp 
+	or time between '2020-02-17 13:54:00'::timestamp and '2020-02-17 15:36:50'::timestamp
+	or time between '2020-02-17 15:40:00'::timestamp and '2020-02-17 19:09:17'::timestamp
+	or time between '2020-02-17 19:17:10'::timestamp and '2020-02-17 23:36:00'::timestamp)
 group by variable, time
 order by time
 ;
@@ -143,8 +195,12 @@ from (select distinct time, value, name, labels from metrics
 where name = 'service:X_container_count:sum' 
 and labels ->> 'label_app' in ('checkoutservice','cartservice','emailservice','currencyservice'
 	,'paymentservice','productcatalogservice','shippingservice')
-and time between '2020-02-17 08:36:00'::timestamp and '2020-02-17 23:36:00'::timestamp
+and (time between '2020-02-17 08:36:21'::timestamp and '2020-02-17 12:22:17'::timestamp 
+	or time between '2020-02-17 13:54:00'::timestamp and '2020-02-17 15:36:50'::timestamp
+	or time between '2020-02-17 15:40:00'::timestamp and '2020-02-17 19:09:17'::timestamp
+	or time between '2020-02-17 19:17:10'::timestamp and '2020-02-17 23:36:00'::timestamp)
 group by variable, time
+order by time
 ;
 
 select 'svc_err' as variable, time, sum(round(cast(COALESCE(value,'NaN') as numeric) ,2)) as val
@@ -154,7 +210,10 @@ where name = 'service:X_requests_error_total:sum_rate'
 and labels ->> 'destination_service' in ('checkoutservice.default.svc.cluster.local','cartservice.default.svc.cluster.local'
     ,'emailservice.default.svc.cluster.local','currencyservice.default.svc.cluster.local','paymentservice.default.svc.cluster.local'
     ,'productcatalogservice.default.svc.cluster.local','shippingservice.default.svc.cluster.local')
-and time between '2020-02-17 08:36:00'::timestamp and '2020-02-17 23:36:00'::timestamp
+and (time between '2020-02-17 08:36:21'::timestamp and '2020-02-17 12:22:17'::timestamp 
+	or time between '2020-02-17 13:54:00'::timestamp and '2020-02-17 15:36:50'::timestamp
+	or time between '2020-02-17 15:40:00'::timestamp and '2020-02-17 19:09:17'::timestamp
+	or time between '2020-02-17 19:17:10'::timestamp and '2020-02-17 23:36:00'::timestamp)
 group by variable, time
 order by time
 ;
