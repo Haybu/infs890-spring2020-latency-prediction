a21ec91d014bceed3daf2efd06cf93bab86a5b41
Haytham Mohamed <haybu@hotmail.com>
2020-02-21 14:35:34 -0600

    update

---
+++ data/raw-data-bad/readme.md
@@ -0,0 +1,3 @@
+This is data is not good. Queries are selecting duplicates records. 
+
+DO NOT USE
--- scale-deployments.sh
+++ scale-deployments.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-kubectl scale deployment.v1.apps/cartservice --replicas=1
-kubectl scale deployment.v1.apps/checkoutservice --replicas=1
-kubectl scale deployment.v1.apps/currencyservice --replicas=1
-kubectl scale deployment.v1.apps/emailservice --replicas=1
-kubectl scale deployment.v1.apps/paymentservice --replicas=1
-kubectl scale deployment.v1.apps/productcatalogservice --replicas=1
-kubectl scale deployment.v1.apps/shippingservice --replicas=1
-#kubectl scale deployment.v1.apps/recommendationservice --replicas=1
+kubectl scale deployment.v1.apps/cartservice --replicas=5
+kubectl scale deployment.v1.apps/checkoutservice --replicas=5
+kubectl scale deployment.v1.apps/currencyservice --replicas=5
+kubectl scale deployment.v1.apps/emailservice --replicas=5
+kubectl scale deployment.v1.apps/paymentservice --replicas=5
+kubectl scale deployment.v1.apps/productcatalogservice --replicas=5
+kubectl scale deployment.v1.apps/shippingservice --replicas=5
+kubectl scale deployment.v1.apps/recommendationservice --replicas=5
 
 echo "done."
